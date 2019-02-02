cf3108690df4fefa6eda366383b0270081fd0111
Geoffrey <geoffreyplichard@gmail.com>
2019-02-02 15:45:24 +0100

    get the token color related to the player

--- src/app/token/token.component.ts
+++ src/app/token/token.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-token',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./token.component.scss']
 })
 export class TokenComponent implements OnInit {
+
+  @Input() tokenColor: string;
 
   constructor() { }
 
